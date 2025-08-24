19d645f2aa1e28534f446a6561ced713eea6875a
Tian <66271788+TianYu-00@users.noreply.github.com>
2025-08-24 21:27:10 +0100

    update settings.lua + fix repeat_key_spinner duplicated instead of menu_button_toggle_spinner

--- scripts/widgets/settings.lua
+++ scripts/widgets/settings.lua
@@ -8,9 +8,11 @@ local SettingsSpinner = require("widgets/settingsspinner")
 local DefaultSettings = require("defaultsettings")
 local json = require("json")
 local Image = require("widgets/image")
+local DebugLog = require("utils/debug")
 
 local Settings = Class(Widget, function(self, context)
 	Widget._ctor(self, TIAN_WHEREISIT_GLOBAL_DATA.IDENTIFIER.WIDGET_SETTINGS)
+	DebugLog("Settings: Initialized")
 	self.parent_screen = context.screen
 	self.is_open = false
 	self.settings_data = deepcopy(DefaultSettings)
@@ -19,22 +21,26 @@ local Settings = Class(Widget, function(self, context)
 end)
 
 function Settings:CreateMenu()
+	DebugLog("Settings: Creating menu")
 	self.bg = self.menu_container:AddChild(Templates.CurlyWindow(400, 450, 1, 1, 68, -40))
 	self.bg:SetTint(1, 1, 1, 0.7)
 
 	self.save_button = self.menu_container:AddChild(Templates2.StandardButton(function()
+		DebugLog("Settings: Save button clicked")
 		self:SaveSettings()
 	end, TIAN_WHEREISIT_GLOBAL_DATA.STRINGS.SAVE))
 	self.save_button:SetScale(0.4)
 	self.save_button:SetPosition(-150, -200, 0)
 
 	self.reset_button = self.menu_container:AddChild(Templates2.StandardButton(function()
+		DebugLog("Settings: Reset button clicked")
 		self:ResetToDefaults()
 	end, TIAN_WHEREISIT_GLOBAL_DATA.STRINGS.RESET))
 	self.reset_button:SetScale(0.4)
 	self.reset_button:SetPosition(0, -200, 0)
 
 	self.back_button = self.menu_container:AddChild(Templates2.StandardButton(function()
+		DebugLog("Settings: Back button clicked")
 		self:CloseMenu()
 	end, TIAN_WHEREISIT_GLOBAL_DATA.STRINGS.BACK))
 	self.back_button:SetScale(0.4)
@@ -58,12 +64,14 @@ function Settings:CreateMenu()
 	self.description:EnableWordWrap(true)
 
 	self:CreateSpinner()
+	DebugLog("Settings: Menu created")
 end
 
 function Settings:GetSettings(callback)
+	DebugLog("Settings: Loading settings")
 	TheSim:GetPersistentString(TIAN_WHEREISIT_GLOBAL_DATA.IDENTIFIER.PERSIST_SETTINGS, function(success, str)
 		if not success or str == nil or str == "" then
-			-- No file or empty, use defaults
+			DebugLog("Settings: No settings file found, using defaults")
 			self.settings_data = deepcopy(DefaultSettings)
 			SavePersistentString(
 				TIAN_WHEREISIT_GLOBAL_DATA.IDENTIFIER.PERSIST_SETTINGS,
@@ -71,12 +79,12 @@ function Settings:GetSettings(callback)
 				false
 			)
 		else
-			-- Try decode
 			local ok, data = pcall(json.decode, str)
 			if ok and data then
+				DebugLog("Settings: Settings loaded successfully")
 				self.settings_data = data
 			else
-				print("Failed to decode settings, resetting to defaults")
+				DebugLog("Settings: Failed to decode settings, resetting to defaults")
 				self.settings_data = deepcopy(DefaultSettings)
 				SavePersistentString(
 					TIAN_WHEREISIT_GLOBAL_DATA.IDENTIFIER.PERSIST_SETTINGS,
@@ -93,6 +101,7 @@ function Settings:GetSettings(callback)
 end
 
 function Settings:SaveSettings()
+	DebugLog("Settings: Saving settings")
 	if self.settings_data then
 		SavePersistentString(
 			TIAN_WHEREISIT_GLOBAL_DATA.IDENTIFIER.PERSIST_SETTINGS,
@@ -102,8 +111,9 @@ function Settings:SaveSettings()
 		TIAN_WHEREISIT_GLOBAL_DATA.SETTINGS = self.settings_data
 		TIAN_WHEREISIT_GLOBAL_FUNCTION.UpdateKeyBindings()
 		TIAN_WHEREISIT_GLOBAL_FUNCTION.TOGGLE_MENU_BUTTON()
+		DebugLog("Settings: Settings saved")
 	else
-		print("No settings_data to save, writing defaults")
+		DebugLog("Settings: No settings_data to save, writing defaults")
 		self.settings_data = deepcopy(DefaultSettings)
 		SavePersistentString(
 			TIAN_WHEREISIT_GLOBAL_DATA.IDENTIFIER.PERSIST_SETTINGS,
@@ -114,6 +124,7 @@ function Settings:SaveSettings()
 end
 
 function Settings:ResetToDefaults()
+	DebugLog("Settings: Resetting to defaults")
 	self.settings_data = deepcopy(DefaultSettings)
 	if self.spinner_container then
 		self.spinner_container:Kill()
@@ -122,10 +133,10 @@ function Settings:ResetToDefaults()
 end
 
 function Settings:CreateSpinner()
+	DebugLog("Settings: Creating spinners")
 	self.spinner_container = self.menu_container:AddChild(Widget("spinner_container"))
 	self.spinner_container:SetPosition(0, 100, 0)
 
-	-- Menu Key
 	self.menu_key_spinner = self.spinner_container:AddChild(SettingsSpinner({ screen = self }, {
 		label = TIAN_WHEREISIT_GLOBAL_DATA.STRINGS.MENU_KEY,
 		description = TIAN_WHEREISIT_GLOBAL_DATA.STRINGS.MENU_KEY_DESCRIPTION,
@@ -133,7 +144,7 @@ function Settings:CreateSpinner()
 		options = CustomKeyList,
 		default = self.settings_data.MENU_KEY,
 		on_changed = function(value)
-			print("Menu key changed to:", value)
+			DebugLog("Settings: Menu key changed to: " .. tostring(value))
 			self.settings_data.MENU_KEY = value
 		end,
 		width = 300,
@@ -141,7 +152,6 @@ function Settings:CreateSpinner()
 	}))
 	self.menu_key_spinner:SetPosition(0, 0)
 
-	-- Repeat Key
 	self.repeat_key_spinner = self.spinner_container:AddChild(SettingsSpinner({ screen = self }, {
 		label = TIAN_WHEREISIT_GLOBAL_DATA.STRINGS.REPEAT_KEY,
 		description = TIAN_WHEREISIT_GLOBAL_DATA.STRINGS.REPEAT_KEY_DESCRIPTION,
@@ -149,28 +159,29 @@ function Settings:CreateSpinner()
 		options = CustomKeyList,
 		default = self.settings_data.REPEAT_LOOKUP_KEY,
 		on_changed = function(value)
-			print("Repeat key changed to:", value)
+			DebugLog("Settings: Repeat key changed to: " .. tostring(value))
 			self.settings_data.REPEAT_LOOKUP_KEY = value
 		end,
 	}))
 	self.repeat_key_spinner:SetPosition(0, -50)
 
-	-- Menu Button Toggle
-	self.repeat_key_spinner = self.spinner_container:AddChild(SettingsSpinner({ screen = self }, {
+	self.menu_button_toggle_spinner = self.spinner_container:AddChild(SettingsSpinner({ screen = self }, {
 		label = TIAN_WHEREISIT_GLOBAL_DATA.STRINGS.MENU_BUTTON_TOGGLE,
 		description = TIAN_WHEREISIT_GLOBAL_DATA.STRINGS.MENU_BUTTON_TOGGLE_DESCRIPTION,
 		description_widget = self.description,
 		options = { "true", "false" },
 		default = self.settings_data.MENU_BUTTON_TOGGLE,
 		on_changed = function(value)
-			print("Toggle toggeled:", value)
+			DebugLog("Settings: Menu button toggle changed to: " .. tostring(value))
 			self.settings_data.MENU_BUTTON_TOGGLE = value
 		end,
 	}))
-	self.repeat_key_spinner:SetPosition(0, -100)
+	self.menu_button_toggle_spinner:SetPosition(0, -100)
+	DebugLog("Settings: Spinners created")
 end
 
 function Settings:OpenMenu()
+	DebugLog("Settings: Opening menu")
 	self.is_open = true
 	self.parent_screen.proot:Hide()
 	self.parent_screen.sroot:Show()
@@ -181,13 +192,13 @@ function Settings:OpenMenu()
 		self.spinner_container:Kill()
 	end
 
-	-- Load settings and then create spinner
 	self:GetSettings(function()
 		self:CreateSpinner()
 	end)
 end
 
 function Settings:CloseMenu()
+	DebugLog("Settings: Closing menu")
 	self.is_open = false
 	self.parent_screen.proot:Show()
 	self.parent_screen.sroot:Hide()
@@ -195,9 +206,11 @@ function Settings:CloseMenu()
 end
 
 function Settings:CreateSettingsButton()
+	DebugLog("Settings: Creating settings button")
 	local button_atlas = "images/button_icons.xml"
 	local button_tex = "configure_mod.tex"
 	local button = Templates2.IconButton(button_atlas, button_tex, "", "", "", function()
+		DebugLog("Settings button clicked")
 		if self.parent_screen.sroot:IsVisible() then
 			self:CloseMenu()
 		else
@@ -207,7 +220,6 @@ function Settings:CreateSettingsButton()
 
 	button:SetScale(0.5)
 
-	-- Set up tooltips
 	button:SetOnGainFocus(function()
 		self.parent_screen.tooltip_root:UpdatePosition(button, 0, -25)
 		self.parent_screen.tooltip_root.tooltip:SetString(TIAN_WHEREISIT_GLOBAL_DATA.STRINGS.SETTINGS)
