7016727cd70d259f2d773c2bc67670aeabd9848e
Tian <66271788+TianYu-00@users.noreply.github.com>
2025-08-24 20:09:54 +0100

    Update entitysearch.lua

--- scripts/widgets/entitysearch.lua
+++ scripts/widgets/entitysearch.lua
@@ -1,18 +1,20 @@
 local Widget = require("widgets/widget")
 local Templates2 = require("widgets/redux/templates")
+local DebugLog = require("utils/debug")
 
 local EntitySearch = Class(Widget, function(self, context)
 	Widget._ctor(self, TIAN_WHEREISIT_GLOBAL_DATA.IDENTIFIER.WIDGET_ENTITY_SEARCH)
 	self.parent_screen = context.screen
+	DebugLog("EntitySearch: Initialized")
 end)
 
--- This needs to be moved to entitysearch.lua
 function EntitySearch:FilterEntityList(search)
+	DebugLog("EntitySearch: FilterEntityList called with -> '" .. tostring(search) .. "'")
 	local search_lower = search:lower():gsub("^%s*(.-)%s*$", "%1")
 	self.parent_screen.entity_list = {}
 
 	if search_lower == "" then
-		-- Reset to full list
+		DebugLog("EntitySearch: Resetting entity list to full master list")
 		for _, e in ipairs(self.parent_screen.master_entity_list) do
 			table.insert(self.parent_screen.entity_list, e)
 		end
@@ -22,16 +24,13 @@ function EntitySearch:FilterEntityList(search)
 			local custom_match = entity.custom_name and entity.custom_name:lower():find(search_lower, 1, true)
 			if name_match or custom_match then
 				table.insert(self.parent_screen.entity_list, entity)
+				DebugLog("EntitySearch: Matched entity -> " .. entity.name)
 			end
 		end
 	end
 
+	DebugLog("EntitySearch: Filtered entity list contains " .. tostring(#self.parent_screen.entity_list) .. " entities")
 	self.parent_screen:CreateEntityList()
 end
 
 return EntitySearch
-
------------------------------------ Comments -----------------------------------
-
--- This search widget isnt really needed as i added same filter logic on input directly
--- but ill leave it here just in case
