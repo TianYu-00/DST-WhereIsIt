5785ad334a097312287a0dfa52fad6d751e5841b
Tian <66271788+TianYu-00@users.noreply.github.com>
2025-08-24 20:10:46 +0100

    Update hudbutton.lua

--- scripts/widgets/hudbutton.lua
+++ scripts/widgets/hudbutton.lua
@@ -2,9 +2,11 @@ local Widget = require("widgets/widget")
 local Templates = require("widgets/templates")
 local ImageButton = require("widgets/imagebutton")
 local Text = require("widgets/text")
+local DebugLog = require("utils/debug")
 
 local HudButton = Class(Widget, function(self, context)
 	Widget._ctor(self, TIAN_WHEREISIT_GLOBAL_DATA.IDENTIFIER.WIDGET_HUD_BUTTON)
+	DebugLog("HudButton: Initialized")
 
 	self.hud_button_root = self:AddChild(Widget("hud_button_container"))
 
@@ -13,13 +15,15 @@ local HudButton = Class(Widget, function(self, context)
 end)
 
 function HudButton:CreateButton()
+	DebugLog("HudButton: Creating button")
 	self.hud_button_root:SetHAnchor(ANCHOR_RIGHT)
 	self.hud_button_root:SetVAnchor(ANCHOR_BOTTOM)
 	self.hud_button_root:SetScaleMode(SCALEMODE_PROPORTIONAL)
-	self.hud_button_root:SetPosition(-270, 35) -- x,y
+	self.hud_button_root:SetPosition(-270, 35)
 
 	self.bg = self.hud_button_root:AddChild(ImageButton("images/ui.xml", "button_small.tex"))
 	self.bg:SetOnClick(function()
+		DebugLog("HudButton: Clicked, toggling menu")
 		TIAN_WHEREISIT_GLOBAL_FUNCTION.TOGGLE_MENU()
 	end)
 	self.bg:SetFocusScale(1.1, 0.8)
@@ -30,13 +34,17 @@ function HudButton:CreateButton()
 	self.bg:SetTextFocusColour(unpack(WHITE))
 	self.bg:SetTextSize(20)
 	self.bg.image:SetTint(0, 0, 0, 0.5)
+	DebugLog("HudButton: Button created")
 end
 
 function HudButton:ToggleButton()
+	DebugLog("HudButton: ToggleButton called")
 	if TIAN_WHEREISIT_GLOBAL_DATA.SETTINGS.MENU_BUTTON_TOGGLE == "true" then
 		self.hud_button_root:Show()
+		DebugLog("HudButton: Button shown")
 	else
 		self.hud_button_root:Hide()
+		DebugLog("HudButton: Button hidden")
 	end
 end
 
